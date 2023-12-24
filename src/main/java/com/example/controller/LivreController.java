6602ce83a43fceae8b47748a072c15267a96948c
mariem <86363907+MarieMoalla@users.noreply.github.com>
2023-12-24 14:01:29 +0100

    add ajout livre template

--- src/main/java/com/example/controller/LivreController.java
+++ src/main/java/com/example/controller/LivreController.java
@@ -6,6 +6,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
@@ -19,19 +22,43 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/livres")
 @Api(value = "Livre Controller")
 public class LivreController {
 
 	@Autowired
     private LivreService livreService;
 	
-	@GetMapping("/livres")
+	@GetMapping
     @ApiOperation(value = "Cette opération nous permet de recevoir la liste des livres")
-    public ModelAndView getLivres(Model model) {
+    public ModelAndView getLivres(Model model) 
+	{
         List<Livre> livres = livreService.getLivres();
         model.addAttribute("livres", livres);
-        return new ModelAndView("ListesLivre", model.asMap());
+        return new ModelAndView("LivreTemplates/ListesLivre", model.asMap());
+    }
+	
+	/* Form create   */ 
+    @GetMapping("/createLivreForm")
+    @ApiOperation(value = "Afficher le formulaire d'ajout d'un livre")
+    public ModelAndView getCreateAuteurForm(Model model) {
+        Livre nouvelLivre = new Livre();
+        model.addAttribute("nouvelLivre", nouvelLivre);
+        return new ModelAndView("LivreTemplates/ajoutLivre", model.asMap());
+    }
+    /* Action Create*/
+	@PostMapping("/createLivre")
+    @ApiOperation(value = "Cette opération nous permet d'ajouter un livre")
+    public ModelAndView createAuteur(@ModelAttribute("nouvelLivre") Livre livre) 
+	{
+        livreService.saveLivre(livre);
+        return new ModelAndView("redirect:/api/livres/"+livre.getLivre_id());
+    }
+	
+    @GetMapping("/{id}")
+    @ApiOperation(value = "Cette opération nous permet de retourner un auteur demandé")
+    public Livre getAuteur(@PathVariable Long id) {
+        return livreService.getLivre(id);
     }
 	
 }
